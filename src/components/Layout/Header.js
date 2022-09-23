2a6e651471484f87f43b480cc0a6877b9a15a84e
mohamdallam <mohamedahmedallam1@gmail.com>
2022-09-24 00:46:55 +0200

    init

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import classes from "./Header.module.css";
+
+import mealsImages from "../../assets/meals.jpg";
+import HeaderCardButton from "./HeaderCardButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCardButton />
+      </header>
+
+      <div className={classes["main-image"]}>
+        <img src={mealsImages} alt="" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
